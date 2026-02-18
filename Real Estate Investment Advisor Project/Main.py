fc366098f6667537fd0355cb3b371cc523ed2604
Josphia <josphiagladlin@gmail.com>
2026-02-18 22:37:46 +0530

    Encoded

--- Real Estate Investment Advisor Project/Main.py
+++ Real Estate Investment Advisor Project/Main.py
@@ -1,3 +1,20 @@
-print("Hii")
-print("Hello")
-print("Hii Vijilee")
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv(r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\india_housing_prices.csv")
+
+#res= df.head()
+#res = df.info()
+#res = df.isnull().sum()
+
+#df.drop_duplicates(inplace=True)
+#res = df.info()
+
+#print(df.describe())
+
+le = LabelEncoder()
+
+df['City_Encoded'] = le.fit_transform(df['City'])
+df['Property_Type_Encoded'] = le.fit_transform(df['Property_Type'])
+
+print(df.head())
