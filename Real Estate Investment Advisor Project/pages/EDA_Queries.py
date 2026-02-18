262336d97dcd6368ccb3e0703ffb18ab88137dfb
Josphia <josphiagladlin@gmail.com>
2026-02-19 01:03:33 +0530

    Query 1

--- Real Estate Investment Advisor Project/pages/EDA_Queries.py
+++ Real Estate Investment Advisor Project/pages/EDA_Queries.py
@@ -11,6 +11,7 @@ st.set_page_config(
 )
 
 st.title("🔍 Property Query Page")
+df = pd.read_csv(r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\india_housing_prices.csv")
 
 option = st.selectbox("", options = [
     "1. What is the distribution of property prices?", 
@@ -18,4 +19,17 @@ option = st.selectbox("", options = [
     "3. How does the price per sq ft vary by property type?", 
     "4. Is there a relationship between property size and price?"], index=None, placeholder="Select One Josphia")
 
-st.write(f"You have selected - {option}")
+
+if option == "1. What is the distribution of property prices?":
+    st.title("📊 EDA - Property Price Distribution")
+    fig, ax = plt.subplots(figsize=(8,5))
+    sns.histplot( df['Price_in_Lakhs'], bins=25, kde=True, ax=ax, color="#ffc6c6" )
+    ax.set_title("Distribution of Property Prices")
+    ax.set_xlabel("Price (in Lakhs)")
+    ax.set_ylabel("Number of Properties")
+    st.pyplot(fig)  
+
+
+
+
+
