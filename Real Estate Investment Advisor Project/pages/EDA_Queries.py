59600568ce182868cf016cac2e632ee1a603eec5
Josphia <josphiagladlin@gmail.com>
2026-02-19 00:19:57 +0530

    Query Page

--- Real Estate Investment Advisor Project/pages/EDA_Queries.py
+++ Real Estate Investment Advisor Project/pages/EDA_Queries.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 st.set_page_config(
     page_title="EDA: Query Page",
@@ -9,4 +12,10 @@ st.set_page_config(
 
 st.title("🔍 Property Query Page")
 
-st.write("Here users can enter property details and get predictions.")
+option = st.selectbox("", options = [
+    "1. What is the distribution of property prices?", 
+    "2. What is the distribution of property sizes?", 
+    "3. How does the price per sq ft vary by property type?", 
+    "4. Is there a relationship between property size and price?"], index=None, placeholder="Select One Josphia")
+
+st.write(f"You have selected - {option}")
