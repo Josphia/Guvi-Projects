bb43fd8a48f973b0c461e9c88a1a69577dbaa096
Josphia <josphiagladlin@gmail.com>
2026-02-20 22:46:24 +0530

    CC

--- Real Estate Investment Advisor Project/pages/Good_Investment.py
+++ Real Estate Investment Advisor Project/pages/Good_Investment.py
@@ -3,9 +3,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression 
+from xgboost import XGBClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 
-# 1. Load the dataset
 df = pd.read_csv(r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\india_housing_prices.csv")
 
 city_median = df.groupby('City')['Price_per_SqFt'].transform('median')
@@ -27,10 +30,30 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train_scaled, y_train)
 
-predictions = model.predict(X_test_scaled)
-print("\n--- Model Performance ---")
-print(f"Accuracy: {accuracy_score(y_test, predictions):.2f}")
-print("\nClassification Report:\n", classification_report(y_test, predictions))
+models_list = {
+    "Random Forest": RandomForestClassifier(n_estimators=100, random_state=42),
+    "Decision Tree": DecisionTreeClassifier(random_state=42),
+    "Logistic Regression": LogisticRegression(max_iter=1000, random_state=42),
+    "XGBoost": XGBClassifier(eval_metric='logloss', random_state=42),
+    "SVM": SVC(probability=True, random_state=42)
+}
+
+for name, model in models_list.items():
+    print(f"Training {name}...")
+    
+    model.fit(X_train_scaled, y_train)    # Training the actual object
+    preds = model.predict(X_test_scaled)  # Making predictions
+    acc = accuracy_score(y_test, preds)   # Checking accuracy
+    
+    print(f"{name} Accuracy: {acc:.4f}")
+    print("-" * 30)
+
+
+
+
+
+
+
+
+
