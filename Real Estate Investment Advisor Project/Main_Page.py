cff9f7348b16054d7f075a4f7e09cc9cb2047bd5
Josphia <josphiagladlin@gmail.com>
2026-02-24 23:13:59 +0530

    f1

--- Real Estate Investment Advisor Project/Main_Page.py
+++ Real Estate Investment Advisor Project/Main_Page.py
@@ -2,6 +2,17 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 
+df = pd.read_csv(r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\india_housing_prices.csv")
+
+#res= df.head()
+#res = df.info()
+#res = df.isnull().sum()
+
+#df.drop_duplicates(inplace=True)
+#res = df.info()
+
+#print(df.describe())
+
 st.set_page_config(
     page_title="Real Estate Investment Advisor",
     page_icon="📈",
@@ -11,7 +22,7 @@ st.set_page_config(
 st.title("India Real Estate Investment Advisor")
 st.write("Analyzing 250,000 property records...")
 
-df = pd.read_csv(r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\india_housing_prices.csv")
+
 
 #st.dataframe(df)
 
