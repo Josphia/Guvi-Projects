f85847a71825704a9d9b0823b9e7ce0214a5f487
Josphia <josphiagladlin@gmail.com>
2026-02-24 23:42:10 +0530

    f2

--- Real Estate Investment Advisor Project/train_classification.py
+++ Real Estate Investment Advisor Project/train_classification.py
@@ -2,6 +2,9 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import mlflow
+import mlflow.sklearn
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
@@ -41,11 +44,34 @@ models = {
     
 }
 
+mlflow.set_experiment("Good_Investment_Classification")
+
 trained_models = {}
 
 for name, model in models.items():
-    model.fit(X_train_scaled, y_train)
-    trained_models[name] = model
+
+    with mlflow.start_run(run_name=name):
+
+        model.fit(X_train_scaled, y_train)
+
+        y_pred = model.predict(scaler.transform(X_test))
+        y_proba = model.predict_proba(scaler.transform(X_test))[:, 1]
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        roc_auc = roc_auc_score(y_test, y_proba)
+
+        mlflow.log_param("model_name", name)
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("roc_auc", roc_auc)
+
+        mlflow.sklearn.log_model(model, "model")
+
+        trained_models[name] = model
 
 joblib.dump(features, r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\features.pkl")
 joblib.dump(le_property, r"E:\VS Code Projects\Guvi-Projects\Real Estate Investment Advisor Project\le_property.pkl")
